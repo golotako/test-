56f66f332f0eb470ae6e736d1ec87b86a8970d71
golotako <97626687+golotako@users.noreply.github.com>
2022-02-17 19:40:15 +0200

    Create bonus.py

---
+++ bonus.py
@@ -0,0 +1,41 @@
+# bonus
+
+'''
+list is a collection of ordered data.
+
+tuple is an ordered collection of data.
+
+set is an unordered collection.
+
+'''
+
+
+# another bonus
+
+str1 = '*'
+str2=  '***'
+str3=  '******'
+str4=  ''
+str5=  '*****'
+str6=  '***'
+str7=  '*'
+
+#take number from user and print it in str4 
+
+user_number=int(input('enter a number: '))
+
+# add the number to str4
+
+str4= str4+str(user_number)
+
+print(str1)
+print(str2)
+print(str3)
+print(str4)
+print(str5)
+print(str6)
+print(str7)
+
+
+
+
