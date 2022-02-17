05a080849ba7f6a10cf0640da077e79f7c7abb6e
golotako <97626687+golotako@users.noreply.github.com>
2022-02-17 19:55:04 +0200

    Update testjson.py

--- testjson.py
+++ testjson.py
@@ -6,24 +6,28 @@ import json
 
 class Post():
     def __init__(self,userid,id,title,body):
-        self.userid=userid
-        self.id=id
-        self.title=title
-        self.body=body
+            self.userid=userid
+            self.id=id
+            self.title=title
+            self.body=body
+
     def __str__(self):
-        return f'{self.userid} {self.id} {self.title} {self.body}'
+            return f'userid:{self.userid},id: {self.id},title: {self.title},body: {self.body}'
 
     def find_user_by_id():
-        url='https://jsonplaceholder.typicode.com/posts/'
-        response=requests.get(url).json()
-        id=int(input('Enter a id: '))
-        for post in response:
-            url='https://jsonplaceholder.typicode.com/posts/'+str(id)
+            url='https://jsonplaceholder.typicode.com/posts/'
             response=requests.get(url).json()
-            if id==post['id']:
-                print(json.dumps(response,indent=4))
-                break
+            id=int(input('Enter a id: '))
+            for post in response:
+                url='https://jsonplaceholder.typicode.com/posts/'+str(id)
+                response=requests.get(url).json()
+                if id==post['id']:
+                    print(response.__str__())
+                    break
+                
 
 
 
-    
+# test code
+
+Post.find_user_by_id()       
